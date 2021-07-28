267a148b8b301e3f0b59426eac8ce1faeb619503
officialputuid <officialputuid@hack.id>
2021-07-28 18:07:20 +0800

    RMX2151: Adding RealmeDirac blobs

--- RMX2151-vendor.mk
+++ RMX2151-vendor.mk
@@ -5,6 +5,13 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/realme/RMX2151
 
+# Dirac
+PRODUCT_COPY_FILES += \
+    vendor/realme/RMX2151/proprietary/product/vendor_overlay/29/etc/audio_effects.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/audio_effects.xml \
+    vendor/realme/RMX2151/proprietary/product/vendor_overlay/29/etc/diracmobile.config:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/diracmobile.config \
+    vendor/realme/RMX2151/proprietary/product/vendor_overlay/29/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libDiracAPI_SHARED.so \
+    vendor/realme/RMX2151/proprietary/product/vendor_overlay/29/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/soundfx/libdirac.so
+
 PRODUCT_COPY_FILES += \
     vendor/realme/RMX2151/proprietary/bin/kpoc_charger:$(TARGET_COPY_OUT_SYSTEM)/bin/kpoc_charger \
     vendor/realme/RMX2151/proprietary/product/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml \
